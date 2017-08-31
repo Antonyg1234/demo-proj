081b20ce47095cdda58cb70fd1541fcd9d97179b
Antony George <antony.george@neosofttech.com>
2017-08-31 18:35:10 +0530

    swag

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session','form_validation','email','cart','MCAPI');
+$autoload['libraries'] = array('database', 'session','form_validation','email','facebook','cart','MCAPI');
 
 /*
 | -------------------------------------------------------------------
