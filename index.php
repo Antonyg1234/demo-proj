dc3eaa2bd65664f6cf9f75258bba9a8958b8d5fc
Antony George <antony.george@neosofttech.com>
2017-08-31 16:50:45 +0530

    index file error change

--- index.php
+++ index.php
@@ -66,13 +66,13 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
-		// error_reporting(-1);
-		// ini_set('display_errors', 1);
+		 error_reporting(-1);
+		 ini_set('display_errors', 1);
 	break;
 
 	case 'testing':
 	case 'production':
-	    error_reporting(E_ALL);
+	    error_reporting(-1);
 		ini_set('display_errors', 1);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
 		{
