1c1741237ddfa9ebe24e0743ac7e00f57e27a566
Antony George <antony.george@neosofttech.com>
2017-08-31 19:25:58 +0530

    name change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://limitless-reaches-98830.herokuapp.com/';
+$config['base_url'] = 'https://shopcartt.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
