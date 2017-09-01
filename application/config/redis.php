2f466d96ac1d224832df6c29d9f936cc86f2f517
Antony George <antony.george@neosofttech.com>
2017-09-01 16:05:19 +0530

    redis change

--- application/config/redis.php
+++ application/config/redis.php
@@ -7,6 +7,6 @@
  */
 
 // Connection details
-$config['redis_host'] = 'localhost';		// IP address or host
+$config['redis_host'] = '127.0.0.1';		// IP address or host
 $config['redis_port'] = '6379';				// Default Redis port is 6379
 $config['redis_password'] = '';				// Can be left empty when the server does not require AUTH
