34a644a90a454265c829843dfd03ccf4d3ce2607
Antony George <antony.george@neosofttech.com>
2017-09-01 16:11:35 +0530

    redis remove

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session','form_validation','email','facebook','cart','MCAPI','Redis');
+$autoload['libraries'] = array('database', 'session','form_validation','email','facebook','cart','MCAPI');
 
 /*
 | -------------------------------------------------------------------
